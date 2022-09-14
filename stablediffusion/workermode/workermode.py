ae14e112800bf33f0f1f2155ab251f6279829687
Mike Howles <mike.howles@gmail.com>
2022-09-14 17:13:06 -0500

    sampler fix

--- stablediffusion/workermode/workermode.py
+++ stablediffusion/workermode/workermode.py
@@ -122,11 +122,13 @@ def do_run(accelerator, device, model, config, opt):
                         
                         sampler = K.sampling.sample_lms(model_wrap_cfg, x, sigmas, extra_args=extra_args, disable=not accelerator.is_main_process)
                         
-                        if not opt.sampler or opt.sampler == "k_lms":
+                        try:
+                            if opt.sampler == "k_lms":
+                                sampler = K.sampling.sample_lms(model_wrap_cfg, x, sigmas, extra_args=extra_args, disable=not accelerator.is_main_process)
+                            if opt.sampler == "k_euler_ancestral":
+                                sampler = K.sampling.sample_euler_ancestral(model_wrap_cfg, x, sigmas, extra_args=extra_args, disable=not accelerator.is_main_process)
+                        except:
                             sampler = K.sampling.sample_lms(model_wrap_cfg, x, sigmas, extra_args=extra_args, disable=not accelerator.is_main_process)
-                        
-                        if opt.sampler == "k_euler_ancestral":
-                            sampler = K.sampling.sample_euler_ancestral(model_wrap_cfg, x, sigmas, extra_args=extra_args, disable=not accelerator.is_main_process)
 
                         x_samples_ddim = model.decode_first_stage(sampler)
                         x_samples_ddim = torch.clamp((x_samples_ddim+1.0)/2.0, min=0.0, max=1.0)
